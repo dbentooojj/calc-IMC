f9b00528186f5870d403faf9a39db18917b729bd
Diogo José Bento <100057646+dbentooojj@users.noreply.github.com>
2022-04-29 16:44:15 -0300

    Create calc-imc.py

---
+++ calc-imc.py
@@ -0,0 +1,9 @@
+from email.policy import default
+from tkinter import *
+
+master = Tk()
+master.title('Calculadora de IMC by DIOGO BENTO')
+master.geometry('490x560+610+153')
+master.resizeble(Widget=1, height=1)
+
+img_fundo = PhotoImage(file="imagens")
